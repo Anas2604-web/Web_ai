3e460dcb23d93bbd0f22e25ddd3116d8fd6566cc
Anas Khan <annaasskhan6@gmail.com>
2025-05-28 01:04:21 +0530

    initial commit

---
+++ components/CompanionCard.tsx
@@ -0,0 +1,16 @@
+interface CompanionCardProps {
+    id : string;
+    name: string;
+    topic : string;
+    subject : string;
+    duration : number;
+    color : string;
+}
+const CompanionCard = ({id, name , topic , subject , duration , color} : CompanionCardProps) => {
+    return (
+       <article key={id} className="companion-card" style={{backgroundColor: color}}>
+
+       </article>
+    )
+}
+export default CompanionCard
