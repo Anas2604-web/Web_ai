d467ce217da8e703429fb1776ccddf6b3a58627a
Anas Khan <annaasskhan6@gmail.com>
2025-07-02 17:52:56 +0530

    Implemented Auth, Billing, Database Setup, Create Companion Server Action

--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import Image from "next/image";
 interface CompanionCardProps {
     id : string;
     name: string;
@@ -9,7 +11,27 @@ interface CompanionCardProps {
 const CompanionCard = ({id, name , topic , subject , duration , color} : CompanionCardProps) => {
     return (
        <article key={id} className="companion-card" style={{backgroundColor: color}}>
+           <div className="flex justify-between items-center">
+                <div className="subject-badge">{subject}</div>
+                <button className="companion-bookmark">
+                    <Image src="/icons/bookmark.svg" alt="bookmark"
+                     width = {12.5} height = {15}
+                    />
+                </button>
+           </div>
 
+           <h2 className="text-2xl font-bold">{name}</h2>
+           <p className="text-sm">{topic}</p>
+           <div className="flex  items-center gap-2">
+                <Image src="/icons/clock.svg" alt="duration" width={13.5} height={13.5}
+                />
+               <p className="text-sm">{duration } minutes</p>
+           </div>
+           <Link className="w-full" href={`/companions/${id}`} >
+               <button className="btn-primary w-full justify-center">
+                   Launch Lesson
+               </button>
+           </Link>
        </article>
     )
 }
